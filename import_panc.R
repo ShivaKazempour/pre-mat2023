c4b5b8bb5af32a7b15866fe6ec728b27da464d25
Shiva Kazempour Dehkordi <56741412+ShivaKazempour@users.noreply.github.com>
2023-08-18 12:01:25 -0500

    Add files via upload

--- import_panc.R
+++ import_panc.R
@@ -21,4 +21,34 @@ png(filename="/Users/kazempour/proj/pre-mat/result/highVar_heatmap.png", height=
 p1=pheatmap(subExper, show_colnames = FALSE, cluster_cols =FALSE)
 p1
 dev.off()
+ 
+#### barplot
+barplot(x)
+barplot(sort(geneVar))
+barplot(geneVar[1:5])
 
+##boxplot
+boxplot(as.numeric(subExper[1, ]))
+##two box plots in one frame
+b1 <- as.numeric(subExper[1, ])
+b2 <- as.numeric(subExper[50, ])
+names(b1) <- rownames(subExper)[1]
+names(b2) <- rownames(subExper)[50]
+boxplot(b1, b2, xlab="Genes", ylab="log10 of expression",
+        names=rownames(subExper)[c(1, 50)])
+
+## violin plots
+g1 <- subExper[1, 1:500] ##1*500
+g2 <- subExper[2, 1:500] ##1*500
+gt1 <- t(g1) ##500*1
+gt2 <- t(g2) ##5001
+g12 <- rbind(gt1, gt2)
+longTable <- data.frame(cbind(g12, genes=rep(c("ACTB", "ACTG1"), each=500)))
+longTable[, "ACTB__chr7"] <- as.numeric(longTable[, "ACTB__chr7"])
+class(longTable[, "ACTB__chr7"])
+ggplot(longTable, aes(x=genes, y=ACTB__chr7, fill=genes))+
+  geom_violin()+ylab("Expression")+ggtitle("violin plots for expression of ACTB and ACTG1 ")
+
+## Q: plot the violin plot of genes CD24__chrY, ANXA2__chr15, TTR__chr18, and
+## TM4SF4__chr3TM4SF4__chr3, for thee first 1,000 cells
+##
